98ff57de7baa8ae2dd5055fe25644636be0c6eed
unknown <56446445+Matsenko@users.noreply.github.com>
2021-07-01 21:21:59 +0300

    75% auth0

--- AnimeOnline/AnimeOnline/urls.py
+++ AnimeOnline/AnimeOnline/urls.py
@@ -21,8 +21,9 @@ urlpatterns = [
     # path('anime_list/',include('anime_list.urls')),
     # path('api/', include('anime_list.urls')),
     path('admin/', admin.site.urls),
-    path('',include('social_django.urls')),
+    path('',include('anime_list.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('/login/auth0',include('social_django.urls')),
     
   
 ]
